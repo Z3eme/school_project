3cb9448a852d5223e310d028630176d5f73e357f
Oliwier Czeremuszkin <98849405+Z3eme@users.noreply.github.com>
2024-02-02 21:14:37 +0100

    changing pages

--- src/app/components/PaginationBar.jsx
+++ src/app/components/PaginationBar.jsx
@@ -1,25 +1,55 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useSearchParams } from 'next/navigation'; 
-import pythonData  from "../resources/text/pythonData.json"
-
+import { useSearchParams } from "next/navigation";
+import pythonData from "../resources/text/pythonData.json";
 
 function PaginationBar() {
   const router = useRouter();
-  const searchParams = useSearchParams()
-  const search = searchParams.get('p')
-  const url = parseInt(search)
+  const searchParams = useSearchParams();
+  const search = searchParams.get("p") || 1;
+  const url = parseInt(search);
 
-  const {pages} = pythonData;
-  let amount = (pages.length);
+  const { pages } = pythonData;
+  const amount = pages.length;
 
-  console.log(amount);
   return (
-    <div>
-      <Link href={{ pathname: router.pathname, query: { p: url + 1 } }}>
-        Go to {url + 1}
-      </Link>
+    <div className="dark:text-[#f5f5f5] text-[#191919] text-center m-auto p-auto">
+      {url > 1 ? (
+        <Link href={{ pathname: router.pathname, query: { p: url - 1 } }}>
+          &lt;
+        </Link>
+      ) : (
+        <span className="dark:text-[#9a9a9a] text-[#535353]">&lt;</span>
+      )}{" "}
+      {url > 1 && url - 1 > 1 && (
+        <Link href={{ pathname: router.pathname, query: { p: 1 } }}>1 ...</Link>
+      )}{" "}
+      {url > 1 && (
+        <Link href={{ pathname: router.pathname, query: { p: url - 1 } }}>
+          {url - 1}
+        </Link>
+      )}{" "}
+      <Link href={{ pathname: router.pathname, query: { p: url } }}>
+        {url}
+      </Link>{" "}
+      {url < amount && (
+        <Link href={{ pathname: router.pathname, query: { p: url + 1 } }}>
+          {url + 1}
+        </Link>
+      )}{" "}
+      {url < amount && url + 1 < amount && (
+        <Link href={{ pathname: router.pathname, query: { p: amount } }}>
+          {"... " + amount}
+        </Link>
+      )}{" "}
+      {url < amount ? (
+        <Link href={{ pathname: router.pathname, query: { p: url + 1 } }}>
+          &gt;
+        </Link>
+      ) : (
+        <span className="dark:text-[#9a9a9a] text-[#535353]">&gt;</span>
+      )}
     </div>
   );
 }
--- src/app/python/learn/page.js
+++ src/app/python/learn/page.js
@@ -2,14 +2,14 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import CodeBox from "../../components/codeBox";
-import Pagging from "../../components/PaginationBar";
+import PaginationBar from "../../components/PaginationBar";
 
 export default function Page() {
   console.log("refresh");
   return (
-    <div>
+    <div className="">
       <Navbar />
-      <Pagging/>
+      <PaginationBar/>
     </div>
   );
 }
